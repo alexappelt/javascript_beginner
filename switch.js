bdafd6e910a913242684c4f6a3d7db98c859ba94
Alex Appelt <alexappelt6501@gmail.com>
2020-04-08 18:23:25 -0300

    switch.js

---
+++ switch.js
@@ -0,0 +1,18 @@
+
+let x=7; //Declaramos x = 7
+
+
+/***
+ * Switch no X, caso ele for 7, exibe a mensagem e break.
+ * Caso X for 6, exibe a mensagem e break;
+ */
+
+switch (x){
+
+    case 7:
+        console.log('X é 7');
+        break;
+    case 6:
+        console.log('X é 6');
+         break;
+}
